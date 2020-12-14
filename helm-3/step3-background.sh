c41301a6d2375d7769a8644d89f9cba608e60427
Laurent Charlois <laurent.charlois@neotys.com>
2020-12-14 12:15:21 +0100

    fix

--- helm-3/step3-background.sh
+++ helm-3/step3-background.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 NODE_PORT=$(kubectl get svc -o jsonpath='{.items[*].spec.ports[?(@.name=="http")].nodePort}')
-export $NGINX_URL=http://[[HOST_SUBDOMAIN]]-$NODE_PORT-[[KATACODA_HOST]].[[KATACODA_DOMAIN]]"
+export NGINX_URL="http://[[HOST_SUBDOMAIN]]-$NODE_PORT-[[KATACODA_HOST]].[[KATACODA_DOMAIN]]"
 echo $NGINX_URL
